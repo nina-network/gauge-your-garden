6c911914ad78fb91396ca286e1e315069d6350ae
nina-network <serenalee357@gmail.com>
2025-09-07 02:58:49 -0400

    class to className syntax error

--- components/Footer.js
+++ components/Footer.js
@@ -8,15 +8,15 @@ export default function Footer() {
         <Navbar bg="white" data-bs-theme="light">
         
           <Nav className="me-auto">
-            <Nav.Link href="#journal"><i class="bi bi-journal"></i></Nav.Link>
-            <Nav.Link href="#investigator"><i class="bi bi-search"></i></Nav.Link>
+            <Nav.Link href="#journal"><i className="bi bi-journal"></i></Nav.Link>
+            <Nav.Link href="#investigator"><i className="bi bi-search"></i></Nav.Link>
           </Nav>
 
           <Navbar.Brand href="#" className={styles.brand}><img src="/favicon_gyg.png" alt="GYG Logo" className={styles.logo} /></Navbar.Brand>
 
           <Nav className="me-auto">
-            <Nav.Link href="#neighborhood"><i class="bi bi-houses"></i></Nav.Link>
-            <Nav.Link href="#profile"><i class="bi bi-person-circle"></i></Nav.Link>
+            <Nav.Link href="#neighborhood"><i className="bi bi-houses"></i></Nav.Link>
+            <Nav.Link href="#profile"><i className="bi bi-person-circle"></i></Nav.Link>
           </Nav>
       </Navbar>
       </footer>
